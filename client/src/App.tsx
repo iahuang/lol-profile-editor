60001217a91b000267c12c6f156953220c41d97a
Ian Huang <iahuang@users.noreply.github.com>
2021-03-20 09:40:18 -0400

    simulated prestige borders

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,51 @@ interface IState {
 
 interface IProps {}
 
+function prestigeBorderColor(level: number) {
+    if (level < 30) {
+        return "#a3aaad";
+    } else if (level < 50) {
+        return "#85ad71";
+    } else if (level < 75) {
+        return "#4687f0";
+    } else if (level < 100) {
+        return "#e8904d";
+    } else if (level < 125) {
+        return "#f0e2ad";
+    } else if (level < 150) {
+        return "#56c77b";
+    } else if (level < 175) {
+        return "#5068f2";
+    } else if (level < 200) {
+        return "#f7c545";
+    } else if (level < 225) {
+        return "#f2f7f7";
+    } else if (level < 250) {
+        return "#5ce082";
+    } else if (level < 275) {
+        return "#6e85f5";
+    } else if (level < 300) {
+        return "#f05f3e"
+    } else if (level < 325) {
+        return "#e9d8ed"
+    } else if (level < 350) {
+        return "#5ce082";
+    } else if (level < 375) {
+        return "#6e85f5";
+    } else if (level < 400) {
+        return "#f05f3e"
+    } else if (level < 425) {
+        return "#f2f7f7";
+    } else if (level < 450) {
+        return "#5ad6a5";
+    } else if (level < 475) {
+        return "#7f46fa";
+    } else if (level < 500) {
+        return "#fa5b46";
+    }
+    return "#ff87d1";
+}
+
 class AppMain extends Component<IProps, IState> {
     summonerInfo: any;
     constructor(props: IState) {
@@ -62,8 +107,8 @@ class AppMain extends Component<IProps, IState> {
                 });
             } else {
                 this.setState({
-                    loading: false
-                })
+                    loading: false,
+                });
             }
         });
     }
@@ -85,7 +130,11 @@ class AppMain extends Component<IProps, IState> {
             return (
                 <div>
                     <div className="profile">
-                        <img className="pfp" src={this.state.pfp}></img>
+                        <img
+                            className="pfp"
+                            src={this.state.pfp}
+                            style={{ border: `4px solid ${prestigeBorderColor(this.summonerInfo.summonerLevel)}` }}
+                        ></img>
                         <div>
                             <span className="display-name">{this.summonerInfo.displayName}</span>
                             <br></br>
@@ -107,7 +156,10 @@ class AppMain extends Component<IProps, IState> {
             <div>
                 {this.makeHeader()}
                 <div className="main">{this.makeMain()}</div>
-                <span className="disclaimer">This site is not affiliated with or endorsed by Riot Games or any of its subsidaries. League of Legends is a registered trademark of Riot Games Inc. Use at your own risk.</span>
+                <span className="disclaimer">
+                    This site is not affiliated with or endorsed by Riot Games or any of its subsidaries. League of
+                    Legends is a registered trademark of Riot Games Inc. Use at your own risk.
+                </span>
             </div>
         );
     }
